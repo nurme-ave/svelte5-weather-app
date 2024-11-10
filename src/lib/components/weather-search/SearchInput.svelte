00bac75d84977b487df3639af2d2f28f2f779e1e
Ave Nurme <nurme.ave@gmail.com>
2024-11-10 12:35:32 +0200

    Still some weird indentation issues

--- src/lib/components/weather-search/SearchInput.svelte
+++ src/lib/components/weather-search/SearchInput.svelte
@@ -1,17 +1,17 @@
 <script>
-	let { input = $bindable(), onInput, onKeydown } = $props();
+  let { input = $bindable(), onInput, onKeydown } = $props();
 </script>
 
 <input
-	type="text"
-	id="city-search"
-	name="city"
-	placeholder="City"
-	bind:value={input}
-	oninput={onInput}
-	onkeydown={onKeydown}
-	autocomplete="off"
-	class="h-16 w-full max-w-[320px] rounded-lg px-4 text-right text-[1.3rem] text-black outline-none
+  type="text"
+  id="city-search"
+  name="city"
+  placeholder="City"
+  bind:value={input}
+  oninput={onInput}
+  onkeydown={onKeydown}
+  autocomplete="off"
+  class="h-16 w-full max-w-[320px] rounded-lg px-4 text-right text-[1.3rem] text-black outline-none
 transition-all duration-200 focus:ring-2 focus:ring-lime-500
 focus:ring-opacity-50 max-[1100px]:text-center"
 />
--- src/lib/utils/weather.js
+++ src/lib/utils/weather.js
@@ -1,14 +1,14 @@
 // Transforms API response into a cleaner format for our components
 export function transformWeatherData(data) {
-	return {
-		city: data.location.name,
-		country: data.location.country,
-		tempC: data.current.temp_c,
-		tempF: data.current.temp_f,
-		condText: data.current.condition.text,
-		condIcon: data.current.condition.icon,
-		windMph: data.current.wind_mph,
-		windKph: data.current.wind_kph,
-		humidity: data.current.humidity
-	};
+  return {
+    city: data.location.name,
+    country: data.location.country,
+    tempC: data.current.temp_c,
+    tempF: data.current.temp_f,
+    condText: data.current.condition.text,
+    condIcon: data.current.condition.icon,
+    windMph: data.current.wind_mph,
+    windKph: data.current.wind_kph,
+    humidity: data.current.humidity
+  };
 }
